4c0879208dbedd4764982d112cb4d5a7b9078a47
ryym <ryym.64@gmail.com>
2020-05-30 20:55:10 +0900

    test(e2e): Run Calmly on another process
    普通に build でやるとなぜか React の Context の挙動がおかしくなる。
    原因わかってない。 Hook violation のエラーは出ないから
    別々の React 使ってる事はないはずだが。

--- test_e2e/e2e.test.ts
+++ test_e2e/e2e.test.ts
@@ -1,18 +1,19 @@
 import * as fs from 'fs';
 import { promisify } from 'util';
+import * as childProcess from 'child_process';
 import * as path from 'path';
 import * as prettier from 'prettier';
-import { build } from '../src/build';
 
-const readdir = promisify(fs.readdir);
+const readDir = promisify(fs.readdir);
 const fileStat = promisify(fs.stat);
 const readFile = promisify(fs.readFile);
+const removeFile = promisify(fs.unlink);
 
 // This excludes directories.
 const readdirRec = async (root: string, intermediate = ''): Promise<string[]> => {
   let allFiles: string[] = [];
   const dirPath = path.join(root, intermediate);
-  const files = await readdir(dirPath);
+  const files = await readDir(dirPath);
   for (let fileName of files) {
     const relPath = path.join(intermediate, fileName);
     const stat = await fileStat(path.join(root, relPath));
@@ -26,14 +27,31 @@ const readdirRec = async (root: string, intermediate = ''): Promise<string[]> =>
   return allFiles;
 };
 
+const spawn = (cmd: string, args: string[]): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    const p = childProcess.spawn(cmd, args);
+    p.stdout.on('data', (data) => {
+      console.log(data);
+    });
+    p.stderr.on('data', (data) => {
+      console.error(data);
+    });
+    p.on('close', (code) => {
+      code === 0 ? resolve() : reject();
+    });
+  });
+};
+
 describe('E2E', () => {
   const e2eDir = path.join(__dirname, 'samples');
+  const runnerPath = path.join(__dirname, 'run.js');
   const testDirs = fs.readdirSync(e2eDir);
 
   for (let testDirName of testDirs) {
     it(testDirName, async () => {
       const testDirPath = path.join(e2eDir, testDirName);
-      await build({ cwd: testDirPath });
+
+      await spawn('node', [runnerPath, testDirPath]);
 
       const distPath = path.join(testDirPath, 'dist');
       const distFiles = await readdirRec(distPath);
